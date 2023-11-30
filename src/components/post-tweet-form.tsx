2d3d5cd7b8def1df0b7044c90bbf0449a91405b7
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-30 21:19:30 +0900

    ✨ [FEAT] 이미지 업로드, 데이터베이스에 이미지 url 저장 기능 구현

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, updateDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
 import { styled } from 'styled-components';
-import { auth, db } from '../firebase';
+import { auth, db, storage } from '../firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const Form = styled.form`
   display: flex;
@@ -84,13 +85,25 @@ export default function PostTweetForm() {
 
     try {
       setLoading(true);
-      await addDoc(collection(db, 'tweets'), {
+      const doc = await addDoc(collection(db, 'tweets'), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || 'Anonymous',
         userId : user.uid,
       })
-    } catch (error) {
+
+      if (file) {
+        const locationRef = ref(storage, `tweets/${user.uid}-${user.displayName}/${doc.id}`);
+        const result = await uploadBytes(locationRef, file);
+        const url = await getDownloadURL(result.ref);
+        await updateDoc(doc, {
+          photo: url
+        })
+      }
+
+      setTweet('');
+      setFile(null);
+    } catch (error) { 
       console.log(error);
     } finally {
       setLoading(false);
@@ -99,7 +112,7 @@ export default function PostTweetForm() {
 
   return (
     <Form onSubmit={onSubmit}>
-      <TextArea rows={5} maxLength={180} value={tweet} onChange={onChange} placeholder='what is happening?!' />
+      <TextArea required rows={5} maxLength={180} value={tweet} onChange={onChange} placeholder='what is happening?!' />
       <AttachFileButton htmlFor='file'>{file ? 'Photo Added ✅' : 'Add Photo'}</AttachFileButton>
       <AttachFileInput onChange={onFileChange} type='file' id='file' accept='image/*' />
       <SubmitButton type='submit' value={isLoading ? 'Posting...' : 'Post Tweet'} />
