9452d4d73faa3d4d3aefb77a3230f235af830497
Largopie <106071687+Largopie@users.noreply.github.com>
2023-11-30 21:51:09 +0900

    ✨ [FEAT] 게시한 글을 바탕으로 타임라인에 나타는 기능 구현

---
+++ src/components/timeline.tsx
@@ -0,0 +1,35 @@
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { styled } from 'styled-components';
+import { db } from '../firebase';
+import Tweet from './tweet';
+
+export interface ITweet {
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+  id: string;
+}
+
+const Wrapper = styled.div``;
+
+export default function Timeline() {
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  const fetchTweets = async () => {
+    const tweetsQuery = query(collection(db, 'tweets'), orderBy('createdAt', 'desc'));
+
+    const snapshot = await getDocs(tweetsQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { photo, tweet, userId, username, createdAt } = doc.data();
+      return { tweet, createdAt, userId, username, photo, id: doc.id };
+    });
+    setTweets(tweets);
+  };
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return <Wrapper>{tweets.map(tweet => <Tweet key={tweet.id} {...tweet} />)}</Wrapper>;
+}
